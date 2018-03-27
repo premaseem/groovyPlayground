f55b9126f80e617ef73bd6144610e8df8d98b58c
Prem <premaseem.webmaster@gmail.com>
2018-03-26 23:21:18 -0500

    exception handling exercise

---
+++ concepts/conditionalStatement/exercise.groovy
@@ -0,0 +1,48 @@
+/*
+@author: Aseem Jain
+We used a class similar to this in a previous exercise but I think it's a short and sweet example of what we need to review in this exercise.
+
+Create An Account Class
+create a property of type BigDecimal called balance with an initial value of 0.0
+Create a method called deposit
+use a conditional structure (if would work great here) to check if the amount being passed is less than zero. If it is we should catch this case because we don't want to deposit negative numbers. In this case throw an exception.
+Create another method called deposit that takes a list of amounts
+use a for loop to loop over these amounts and call deposit
+Now that we have our class let's test it out. You can do all of this in the same file (just don't create a file called Account.groovy)
+
+Create an instance of the account class
+deposit a valid amount
+deposit an invalid amount (what happens?)
+try / catch on invalid amounts
+deposit a list of amounts.
+*/
+
+class NegativeException extends Exception{
+
+}
+
+class Account{
+    BigDecimal balance =0.0
+
+    def deposit(Integer amt){
+        if (amt <0) {
+            throw new NegativeException("Negative amount exception")
+        }
+    }
+
+    def deposit(List amts){
+        amts.each {
+            deposit(it)
+        }
+    }
+
+}
+
+Account act = new Account()
+act.deposit(4)
+try {
+    act.deposit(-2)
+    act.deposit([4,5,-3])
+}catch (NegativeException){
+    println("exception")
+}
