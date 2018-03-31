fdf6e69ca6a5e0114eed386b1e60c46c3ae06a26
Prem <premaseem.webmaster@gmail.com>
2018-03-31 01:51:50 -0500

    Groovy meta programming - Canonical equals, hashCode and toString override :-) all in one go

---
+++ concepts/metaprogramming/MyPerson.groovy
@@ -0,0 +1,11 @@
+package metaprogramming
+
+import groovy.transform.Canonical
+
+
+@Canonical
+class MyPerson {
+    String name
+    Integer age
+}
+
--- concepts/metaprogramming/app.groovy
+++ concepts/metaprogramming/app.groovy
@@ -12,3 +12,9 @@ emp.someMethod()
 println emp.prop1
 //emp.prop2 = "premaseem"
 println emp.prop2
+
+MyPerson p1 = new MyPerson(name:"aseem",age:36)
+MyPerson p2 = new MyPerson(name:"aseem",age:36)
+assert p1.equals(p2)
+assert p1 == p2
+assert p1.hashCode() == p2.hashCode()
