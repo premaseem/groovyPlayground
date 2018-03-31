0953a43f811ebad200c211d64403400c41f59381
Prem <premaseem.webmaster@gmail.com>
2018-03-31 00:27:12 -0500

    Groovy meta programming - metaClass props and methods can be added dynamically using meta class on groovy

--- concepts/metaprogramming/dynamicallyAddProperty.groovy
+++ concepts/metaprogramming/dynamicallyAddProperty.groovy
@@ -11,3 +11,14 @@ e.someMethod = { -> "some method called"}
 
 assert e.prop1 == "prem"
 assert e.someMethod() == "some method called"
+
+class Dev {
+
+}
+
+// props and methods can be added dynamically using meta class on groovy
+Dev d = new Dev()
+d.metaClass.prop1 = "prem"
+d.metaClass.someMethod = { -> "some method called" }
+assert d.someMethod() == "some method called"
+assert d.prop1 == "prem"
