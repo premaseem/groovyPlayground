e2498b13534905ba3f47d1ca2dac824e451710d8
Prem <premaseem.webmaster@gmail.com>
2018-04-01 02:12:26 -0500

    Groovy Builder : Build xml used closures and Markup builders

---
+++ concepts/builders/xml_builder.groovy
@@ -0,0 +1,22 @@
+package builders
+
+import groovy.xml.MarkupBuilder
+
+/*
+@author: Aseem Jain
+@title: xml builder
+*/
+
+MarkupBuilder builder = new MarkupBuilder()
+builder.books{
+    book(isbn:"978-1935182443"){
+        title "Groovy in Action 2nd Edition"
+        author "Dierk Koenig"
+        price 50.58
+    }
+    book(isbn:"978-1935182443"){
+        title "Groovy in Action 2nd Edition"
+        author "Dierk Koenig"
+        price 50.58
+    }
+}
