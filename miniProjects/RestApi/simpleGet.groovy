fbbe63979b298cec40455a7b80873871061176a1
Prem <premaseem.webmaster@gmail.com>
2018-04-01 17:50:38 -0500

    REST in Groovy: http Builder

--- miniProjects/RestApi/simpleGet.groovy
+++ miniProjects/RestApi/simpleGet.groovy
@@ -1,8 +1,19 @@
 package RestApi
 
+//import groovyx.net.http.ContentType
+import groovyx.net.http.RESTClient
 /*
 @author: Aseem Jain
 @title: simple get request from google
 */
 
-println "http://www.google.com".toURL().getText()
+// google weather api call
+
+// https://mvnrepository.com/artifact/org.codehaus.groovy.modules.http-builder/http-builder
+@Grapes(
+        @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
+)
+
+//def client = new RESTClient()
+obj = "http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b6907d289e10d714a6e88b30761fae22".toURL()
+println obj.getText()
