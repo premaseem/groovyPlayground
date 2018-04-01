039a630c5f8e0ff06ff79684de01b10699696210
Prem <premaseem.webmaster@gmail.com>
2018-04-01 18:06:01 -0500

    Groovy File :  List all files in current dir

--- concepts/fileIO/fileGenerator.groovy
+++ concepts/fileIO/fileGenerator.groovy
@@ -1,3 +1,24 @@
 package fileIO
 
-new File("testFile.txt").write("Premaseem is a good boy")
+// creating a file, no need to open close and catch exception
+File file = new File("testFile.txt")
+
+// writing to a file
+file.write("Premaseem is a good boy \n")
+
+// appending to a file
+file.append("Added next line in the file\n")
+
+// Reading from file
+List list = file.readLines()
+list.each { line ->
+    println(line)
+}
+
+// List all files in current dir
+File dir = new File(".")
+dir.eachFile{ dirObj->
+    if (dirObj.isFile()){
+        println dirObj.name
+    }
+}
