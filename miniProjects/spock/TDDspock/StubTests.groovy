7085d6c94a21e6e5aa37d7d2fa1a5fc8dd9c72bd
Prem <premaseem.webmaster@gmail.com>
2018-04-26 18:24:15 -0500

    Spock - Stub tests thrown and notThrown

--- miniProjects/spock/TDDspock/StubTests.groovy
+++ miniProjects/spock/TDDspock/StubTests.groovy
@@ -8,7 +8,7 @@ import spock.lang.Specification
 @link: https://premaseem.wordpress.com/category/computers/design-patterns/
 */
 
-class StubTests extends Specification{
+class StubTests extends Specification {
 
 
     def "should return 2 for method parameter equal to 2"() {
@@ -22,5 +22,21 @@ class StubTests extends Specification{
         list.get(2) == 2
     }
 
+    def "should throw exception if an Integer is added to the list"() {
+        given:
+        List list = Stub()
+        list.add(_ as Integer) >> { throw new IllegalArgumentException() }
 
+        when:
+        list.add(2)
+        then:
+        thrown(IllegalArgumentException)
+
+        when:
+        list.add("String")
+        then:
+        notThrown(IllegalArgumentException)
+
+
+    }
 }
